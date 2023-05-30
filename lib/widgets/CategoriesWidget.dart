5b2126051ac4bddb0a986c333b2e7d54f95fb318
mascahyo1 <mascahyo15@gmail.com>
2023-05-30 17:25:48 +0700

    Initial commit

---
+++ lib/widgets/CategoriesWidget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class CategoriesWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: [
+          for (int i = 1; i < 8; i++)
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 10),
+              padding: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(20)),
+              child:
+                  Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+                Image.asset(
+                  'images/$i.png',
+                  height: 40,
+                  width: 40,
+                ),
+                Text(
+                  'Sandal',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                      color: Color(0xFF4C53A5)),
+                )
+              ]),
+            )
+        ],
+      ),
+    );
+  }
+}
